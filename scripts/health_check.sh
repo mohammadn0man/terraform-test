3226f9b734e66196fe77d7c4bfc51f7179944627
mohammadn0man <mohammad.noman@live.in>
2022-09-18 19:44:45 +0530

    server down identified

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
-
+echo $(date) >> /home/ec2-user/cron_execution_timestamp.log
 PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 curl $PUBLIC_IP
-if [echo $? != 0 ] then
-    echo "hello"
+if [ $(echo "$?") != 0 ]; then
+    touch /home/ec2-user/service_down.txt
+fi;
