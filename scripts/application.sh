80feeeb75669263b2ff2e294688e43b39b41b778
mohammadn0man <mohammad.noman@live.in>
2022-09-18 12:29:28 +0530

    website implemented

--- scripts/application.sh
+++ scripts/application.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 yum update -y
-yum install -y httpd.x86_64
-PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-MAC=$(curl http://169.254.169.254/latest/meta-data/mac)
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-echo $PUBLIC_IP >> /home/ec2-user/test.txt
-echo "$PUBLIC_IP"
+yum install -y httpd.x86_64 git
+git clone https://github.com/mohammadn0man/terraform-test.git -b html-template
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from $PUBLIC_IP, $MAC AND $INSTANCE_ID." > /var/www/html/index.html
+sudo mv -f terraform-test/index.html /var/www/html/index.html
+PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MAC_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/mac)
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+echo "$PUBLIC_IP"
+sudo sed -i "s/INSTANCE_ID/$INSTANCE_ID/g" /var/www/html/index.html
+sudo sed -i "s/PUBLIC_IP/$PUBLIC_IP/g" /var/www/html/index.html
+sudo sed -i "s/MAC_ADDRESS/$MAC_ADDRESS/g" /var/www/html/index.html
